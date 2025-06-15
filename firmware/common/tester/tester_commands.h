a5ef367081efa2cbaeac851ccdd9a36bd1ab18d5
IvanNaum <nuk04@mail.ru>
2025-06-16 00:52:57 +0300

    Bugfix

--- firmware/common/tester/tester_commands.h
+++ firmware/common/tester/tester_commands.h
@@ -4,9 +4,5 @@
 #include "console.h"
 
 void tester_test_command_handler(console_t* state, char* command_line, uint16_t arg_shifts[], uint8_t nargs);
-void tester_test_transistor_command_handler(console_t* state, char* command_line, uint16_t arg_shifts[], uint8_t nargs);
-void tester_test_diode_command_handler(console_t* state, char* command_line, uint16_t arg_shifts[], uint8_t nargs);
-void tester_test_resistor_command_handler(console_t* state, char* command_line, uint16_t arg_shifts[], uint8_t nargs);
-void tester_test_capacitor_command_handler(console_t* state, char* command_line, uint16_t arg_shifts[], uint8_t nargs);
 
 #endif  // TESTER_COMMANDS_H
